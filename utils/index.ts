f609445b9d135534e1447564ff5cea12b4d6c545
Mitesh <ms00110011@gmail.com>
2025-01-28 19:37:43 +0530

    changes api endpoint

--- utils/index.ts
+++ utils/index.ts
@@ -47,12 +47,11 @@ export async function fetchCars(filters: FilterProps) {
   // Set the required headers for the API request
   const headers: HeadersInit = {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   // Set the required headers for the API request
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    `https://api.api-ninjas.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
